7e1e0d3d39a02c5be55658142670fbe3e0af40f8
Axel <axel_clemente@yahoo.com>
2022-09-30 18:18:49 +0200

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -17,7 +17,7 @@ function howManyMovies(movies) {
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(movies) {
     const scores = movies.reduce ((accu, current)=> accu + current.score,0);
-    const averageScore = scores/movies.length;
+    const averageScore =  scores/movies.length;
     return Math.round(averageScore,2);
     
 }
@@ -31,21 +31,57 @@ function dramaMoviesScore(movies) {
     
 }
 
-const testdramaMovies = dramaMoviesScore()
-console.log(testdramaMovies);
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(movie) {
+function orderByYear(movies) {
+    let newMoviesArray = [...movies];
     
-    const yearOrder = movies.sort ((x)=> x.year.sort)
-    return yearOrder
-}
+    newMoviesArray.sort((a, b) => {
+      if (a.year > b.year) {
+        return 1;
+      } else if (b.year > a.year) {
+        return -1;
+      } else {
+        if (a.title > b.title) {
+          return 1;
+        } else if (b.title > a.title) {
+          return -1;
+        }
+        else return 0
+      }
+    });
+    return newMoviesArray;
+  }
+
+
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(movies) {
+    let newOrderAlphabetically = [...movies];
+
+    newOrderAlphabetically.sort ((a, b) => {
+        if (a.title> b.title) {
+            return 1;
+        } else if (b.title> a.title) {
+            return -1;
+        }
+        else return 0
+    });
+    
+    
+     return newOrderAlphabetically.map ((x) => x.title).slice(0,20);
+   
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(movies) {
+    const hoursToMinutes = movies.map ((x) => x.duration);
+    const hoursToMinutessplit = hoursToMinutes.split(1);
+    return hoursToMinutessplit[0] *60;
+    
+}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
